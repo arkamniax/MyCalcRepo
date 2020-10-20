3a339aa8886c59f52cbceac1e07f0e87e91c1e62
Arkadeep Bagal <42431291+arkamniax@users.noreply.github.com>
2020-10-20 11:17:47 +0530

    Update Calc.java

--- Calc.java
+++ Calc.java
@@ -2,7 +2,11 @@ class Calc{
   public static int add(int a, int b){
     return a+b;
   }
+  public static int prod(int a, int b){
+    return a*b;
+  }
   public static void main(String []args){ 
     System.out.println(add(50,20));
+     System.out.println(prod(50,20));
   }
 }
